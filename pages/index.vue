b102acd605bf99af6e2e4fa06cfdabbc8f5b618d
Scott Rod <djmtype@gmail.com>
2022-08-19 17:14:42 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,16 @@ definePageMeta({
 })
 </script> -->
 
+<script>
+export default {
+  head() {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    };
+  },
+};
+</script>
+
 <template>
 <main>
   <NuxtLayout>
